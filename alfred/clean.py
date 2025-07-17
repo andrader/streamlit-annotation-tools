c29d1c0d09972ee6e034bd0a51fea2dfe5ce1e65
Rubens <38709237+andrader@users.noreply.github.com>
2025-07-17 13:20:16 -0300

    chore: refactor project structure by updating MANIFEST.in and migrating build system to setuptools, removing Alfred commands

--- alfred/clean.py
+++
@@ -1,8 +0,0 @@
-import alfred
-
-
-@alfred.command('clean', help='Remove temporary files')
-def clean():
-    rm = alfred.sh('rm')
-
-    alfred.run(rm, ['-rf', 'build', 'dist', '*.egg-info', 'src/streamlit_annotation_tools/frontend/build'])
